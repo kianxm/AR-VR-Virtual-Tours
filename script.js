3972fa596fdf331bc05d756e97ffdfd28df78a7a
kianxm <73912215+kianxm@users.noreply.github.com>
2022-11-06 16:49:46 -0800

    3d room

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const modalBtn = document.querySelector('.dorms-btn');
 const modalBg = document.querySelector('.modal-container');
 const modalClose = document.querySelector('.modal-close');
-const viewDormsBtn = document.getElementsByClassName('.main-btn');
 
 modalBtn.addEventListener('click', () => {
     modalBg.classList.add('modal-active');
@@ -10,3 +9,22 @@ modalBtn.addEventListener('click', () => {
 modalClose.addEventListener('click', () => {
     modalBg.classList.remove('modal-active');
 });
+
+// Room JS
+const h = document.querySelector("#h");
+const b = document.body;
+
+let base = (e) => {
+    var x = e.pageX / window.innerWidth - 0.5;
+    var y = e.pageY / window.innerHeight - 0.5;
+    h.style.transform = `
+        perspective(90vw)
+        rotateX(${ y * 4  + 75}deg)
+        rotateZ(${ -x * 12  + 45}deg)
+        translateZ(-9vw)
+    `;
+}
+
+b.addEventListener("pointermove", base);
+
+
